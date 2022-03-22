ed6ebf81e17210b87ab70ac5efcbf347d59067a3
Shubham Anand Gupta <shubhamanandgupta@outlook.com>
2022-03-22 11:12:11 +0530

    mn

--- i/notes.js
+++ i/notes.js
@@ -1,5 +1,5 @@
 $("body").append([
     `<div>Note :- Here by Default Hindi is set as Typing Language. It can be set to many other Indian Languages.`,
     `<br>Textarea has been used for illustration but Lipi Lekhika also supports input text tag and ContentEditable DIV and SPAN.`,
-    `<br><span class="msg">Read Detailed Documenation of Lipi Lekhika API <a href="https://rebrand.ly/lekhikaapi" target="_blank" class="red">Here</a>.</span></div>`
+    `<br><span class="msg">Read Detailed Documenation of Lipi Lekhika API <a href="https://get.lipilekhika.com/api" target="_blank" class="red">Here</a>.</span></div>`
 ].join(""));
